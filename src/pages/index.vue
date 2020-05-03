05f6a84f116ac6ca6b9eaa5355ede8f64c1bec4a
it-beige <1119514898@qq.com>
2020-05-03 15:04:21 +0800

    首页-广告实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -123,8 +123,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a class="ads-item" :href="`/#/product${item.id}`" v-for="(item, index) in adsList" :key="index">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
     </div>
     <service-bar></service-bar>
   </div>
@@ -232,7 +240,22 @@ export default {
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
-      ]
+      ],
+      adsList:[
+          {
+            id:33,
+            img:'/imgs/ads/ads-1.png'
+          },{
+            id:48,
+            img:'/imgs/ads/ads-2.jpg'
+          },{
+            id:45,
+            img:'/imgs/ads/ads-3.png'
+          },{
+            id:47,
+            img:'/imgs/ads/ads-4.jpg'
+          }
+        ],
     };
   }
 };
@@ -322,6 +345,21 @@ export default {
         height: 100%;
       }
     }
+  }
+
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    .ads-item {
+      width: 296px;
+      height: 167px;
+    }
+
+  }
+
+  .banner {
+    margin-bottom: 50px;
   }
 }
 </style>
