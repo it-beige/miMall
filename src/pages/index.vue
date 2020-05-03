073ab0192f34a13d99787a6b52d00dd6c1ee6ef9
it-beige <1119514898@qq.com>
2020-05-03 14:45:54 +0800

    首页-轮播图实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,327 @@
 <template>
   <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a class="item-name" href="javascript:;">手机 电话卡</a>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="(subItem, index) in item" :key="index">
+                    <a :href="`${subItem ? `/#/product${subItem.id}` : 'javascript:;'}`">
+                      <img :src="`${subItem ? subItem.img : 'imgs/item-box-1.png'}`" alt />
+                      {{`${subItem ? subItem.name : '小米'}`}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-name" href="javascript:;">电视 盒子</a>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="(subItem, index) in item" :key="index">
+                    <a :href="`${subItem ? `/#/product${subItem.id}` : 'javascript:;'}`">
+                      <img :src="`${subItem ? subItem.img : 'imgs/item-box-1.png'}`" alt />
+                      {{`${subItem ? subItem.name : '小米'}`}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-name" href="javascript:;">笔记本 平板</a>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="(subItem, index) in item" :key="index">
+                    <a :href="`${subItem ? `/#/product${subItem.id}` : 'javascript:;'}`">
+                      <img :src="`${subItem ? subItem.img : 'imgs/item-box-1.png'}`" alt />
+                      {{`${subItem ? subItem.name : '小米'}`}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-name" href="javascript:;">家电 插线板</a>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="(subItem, index) in item" :key="index">
+                    <a :href="`${subItem ? `/#/product${subItem.id}` : 'javascript:;'}`">
+                      <img :src="`${subItem ? subItem.img : 'imgs/item-box-1.png'}`" alt />
+                      {{`${subItem ? subItem.name : '小米'}`}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-name" href="javascript:;">出行 穿戴</a>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="(subItem, index) in item" :key="index">
+                    <a :href="`${subItem ? `/#/product${subItem.id}` : 'javascript:;'}`">
+                      <img :src="`${subItem ? subItem.img : 'imgs/item-box-1.png'}`" alt />
+                      {{`${subItem ? subItem.name : '小米'}`}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-name" href="javascript:;">智能 路由器</a>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="(subItem, index) in item" :key="index">
+                    <a :href="`${subItem ? `/#/product${subItem.id}` : 'javascript:;'}`">
+                      <img :src="`${subItem ? subItem.img : 'imgs/item-box-1.png'}`" alt />
+                      {{`${subItem ? subItem.name : '小米'}`}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-name" href="javascript:;">电源 配件</a>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="(subItem, index) in item" :key="index">
+                    <a :href="`${subItem ? `/#/product${subItem.id}` : 'javascript:;'}`">
+                      <img :src="`${subItem ? subItem.img : 'imgs/item-box-1.png'}`" alt="">
+                      {{`${subItem ? subItem.name : '小米'}`}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-name" href="javascript:;">生活 箱包</a>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="(subItem, index) in item" :key="index">
+                    <a :href="`${subItem ? `/#/product${subItem.id}` : 'javascript:;'}`">
+                      <img :src="`${subItem ? subItem.img : 'imgs/item-box-1.png'}`" alt="">
+                      {{`${subItem ? subItem.name : '小米'}`}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <swiper :options="swiperOption">
+          <swiper-slide v-for="(item, index) in slideList" :key="index">
+            <a :href="`/#/product/${item.id}`">
+              <img :src="item.img" />
+            </a>
+          </swiper-slide>
+          <!-- 分页器 -->
+          <div class="swiper-pagination" slot="pagination"></div>
+          <!-- 导航按钮 -->
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads-box"></div>
+      <div class="banner"></div>
+    </div>
     <service-bar></service-bar>
   </div>
-  
 </template>
 
 
 <script>
-import serviceBar from '../components/serviceBar';
+import serviceBar from "../components/serviceBar";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
 export default {
-  name: 'index',
+  name: "index",
   components: {
+    swiper,
+    swiperSlide,
     serviceBar
+  },
+  data() {
+    return {
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        effect: "cube",
+        cubeEffect: {
+          shadowOffset: 100,
+          shadowScale: 0.6
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      },
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg"
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg"
+        },
+        {
+          id: "44",
+          img: "/imgs/slider/slide-3.jpg"
+        },
+        {
+          id: "43",
+          img: "/imgs/slider/slide-4.jpg"
+        },
+        {
+          id: "41",
+          img: "/imgs/slider/slide-5.jpg"
+        }
+      ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9"
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版"
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro"
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区"
+          }
+        ],
+        [
+          {
+            id: 34,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9"
+          },
+          {
+            id: 35,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版"
+          },
+          {
+            id: 36,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro"
+          },
+          {
+            id: 37,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区"
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+    };
   }
-}
+};
 </script>
 
 <style lang="scss">
+@import "../assets/scss/config.scss";
+@import "../assets/scss/mixin.scss";
+.index {
+  .swiper-box {
+    height: 451px;
+    .nav-menu {
+      width: 264px;
+      height: 451px;
+      position: absolute;
+      z-index: 9;
+      background-color: rgba(85, 88, 90, 0.48);
+      box-sizing: border-box;
+      padding: 26px 0;
+      .menu-item {
+        height: 50px;
+        line-height: 50px;
+        &:hover {
+          background-color: $colorA;
+          .children {
+            display: block;
+          }
+        }
+        .item-name {
+          position: relative;
+          display: block;
+          font-size: $fontI;
+          padding-left: 30px;
+          color: $colorG;
+          &::after {
+            content: "";
+            position: absolute;
+            top: 17.5px;
+            right: 30px;
+            @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
+          }
+        }
 
+        .children {
+          display: none;
+          position: absolute;
+          left: 264px;
+          width: 962px;
+          top: 0;
+          height: 451px;
+          background-color: $colorG;
+          border: 1px solid $colorH;
+
+          ul {
+            height: 75px;
+            display: flex;
+            li {
+              height: 75px;
+              line-height: 75px;
+              flex: 1;
+              padding-left: 23px;
+              a {
+                display: block;
+                font-size: $fontJ;
+                color: $colorB;
+                padding-left: 30px;
+                img {
+                  width: 42px;
+                  height: 35px;
+                  margin-right: 15px;
+                  vertical-align: middle;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .swiper-container {
+      height: 451px;
+      .swiper-button-prev {
+        left: 274px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
 </style>
